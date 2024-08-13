fc01704291ce04e4d91724da5b8e8906ebf3f159
grosendo2006 <gonzalo.rosendo@emeritus.org>
2024-08-13 20:30:14 -0300

    first commit

---
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class WebhooksController < ApplicationController
+      def create
+        # request.body
+        byebug
+      end
+    end
+  end
+end
